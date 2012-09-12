6725270d922b3006db1d37b7c2cd64ab35cb90d6
srgblnch <srgblnch@gmail.com>
2012-09-12 13:49:09 +0200

    clean the tester and add optparser

--- testCode.py
+++ testCode.py
@@ -2,7 +2,7 @@
 
 ##############################################################################
 ##
-## file: barCode.py
+## file: testCode.py
 ##
 ## developers history & copyleft: Sergi Blanch-Torne
 ##
@@ -26,6 +26,7 @@
 from armor import Armor32,Armor36
 from GeneralizedRijndael import GeneralizedRijndael
 from random import randint
+from optparse import OptionParser
 
 OFERT_BITS = 17#bits => up to 131071
 BONO_BITS = 16#bits => up to 65535
@@ -117,98 +118,94 @@ def convertKey(keystr):
     else:
         return int(keystr)
 
-def main(arg0,arg1,arg2):
-    if arg1 == "--help":
-        print("\nUse to generate a code: %s --generate=O..O,B..B,A..A --key=H..H [--debug]"%arg0)
-        print("\tWhere, O..O is the ofert number (max: 131071")
-        print("\t       B..B is the bonus number (max: 65535")
-        print("\t       A..A is the attribute    (max: 128)")
-        print("\nUse to validate a code: %s --validate=Z..Z --key=H..H [--debug]"%arg0)
-        print("\tWhere, Z..Z is a code to validate\n")
-        return
-    elif arg1[0].startswith("--generate"):
-        key = convertKey(arg1[1].split("=")[1])
-        generator = BarCode(int(key),arg2)
-        ofert,bonus,attrs = arg1[0].split("=")[1].split(",")
-        code = generator.generateCode(int(ofert), int(bonus), int(attrs))
-        print("\nFor Ofert: %s\tBonus:%s\twith attrs:%s (crc %s)\n\t"\
-              "The code is %s (base36)(len %d)"
-              %(ofert,bonus,attrs,hex(generator.getcrcr()),code.upper(),len(code)))
-    elif arg1[0].startswith("--validate"):
-        key = convertKey(arg1[1].split("=")[1])
-        validator = BarCode(int(key),arg2)
-        code = arg1[0].split("=")[1]
+def generateAndValidateLoop(nloops=NLOOPS,debug=False):
+    kbits = KCOLUMNS*ROWS*WORDSIZE
+    errors = 0
+    codeLenghts = {}
+    for i in range(nloops):
         try:
-            r = validator.validateCode(code)
-        except Exception,e:
-            print("Validation exception: %s"%(e))
-        else:
-            nOfert,nBonus,attrs = r
-            print("Valid: ofert: %d, bonus: %d, attributes: %d, crc: %s\n"
-                  %(nOfert,nBonus,attrs,hex(validator.getcrc())))
-    elif arg1[0].startswith('--loop'):
-        kbits = KCOLUMNS*ROWS*WORDSIZE
-        #key = randint(0,int('0b'+'1'*(kbits),2))#should test different random keys
-        errors = 0
-        codeLenghts = {}
-        for i in range(NLOOPS):
+            err = 0
+            #generate codeslenght
+            key = randint(0,int('0b'+'1'*(kbits),2))
+            tester = BarCode(int(key),debug)
+            ofert = randint(0,131071)
+            bonus = randint(0,65535)
+            attrs = randint(0,127)
+            #FIXME: may exclude if the joints of values has been already tested
+            code = tester.generateCode(ofert,bonus,attrs)
+            print("Ofert: %s\tBonus:%s\twith attrs:%s\tkey:%s\tcrc:%s\n"\
+                  "The code is %s (base36)(len %d)"
+                  %(ofert,bonus,attrs,hex(key),hex(tester.getcrc()),code.upper(),len(code)))
+            if not codeLenghts.has_key(len(code)): codeLenghts[len(code)] = 0
+            codeLenghts[len(code)] +=1            
+            #validates this codes        
             try:
-                err = 0
-                #generate codeslenght
-                key = randint(0,int('0b'+'1'*(kbits),2))
-                tester = BarCode(int(key),arg2)
-                ofert = randint(0,131071)
-                bonus = randint(0,65535)
-                attrs = randint(0,127)
-                #FIXME: may exclude if the joints of values has been already tested
-                code = tester.generateCode(ofert,bonus,attrs)
-                print("Ofert: %s\tBonus:%s\twith attrs:%s\tkey:%s\tcrc:%s\n"\
-                      "The code is %s (base36)(len %d)"
-                      %(ofert,bonus,attrs,hex(key),hex(tester.getcrc()),code.upper(),len(code)))
-                if not codeLenghts.has_key(len(code)): codeLenghts[len(code)] = 0
-                codeLenghts[len(code)] +=1            
-                #validates this codes        
-                try:
-                    r = tester.validateCode(code)
-                except Exception,e:
-                    err +=1
-                    print("Validation exception: %s"%(e))
-                else:
-                    if ofert==r[0] and bonus == r[1] and attrs == r[2]:
-                        print("Well done! code %s (base36) match with "\
-                              "ofert %d,bonus %d, attrs %d, crc %s"
-                              %(code.upper(),r[0],r[1],r[2],hex(tester.getcrc())))
-                if not err == 0: errors += 1
-                print("\n")
-            except: break
-        if not errors == 0:
-            print("\n--There where %d errors!--\n"%(errors))
-        else:
-            print("\n--No errors found--\n")
-        for lenght in codeLenghts.keys():
-            print("Base 36 have had %d codes with lenght %d\n"%(codeLenghts[lenght],lenght))
-            
+                r = tester.validateCode(code)
+            except Exception,e:
+                err +=1
+                print("Validation exception: %s"%(e))
+            else:
+                if ofert==r[0] and bonus == r[1] and attrs == r[2]:
+                    print("Well done! code %s (base36) match with "\
+                          "ofert %d,bonus %d, attrs %d, crc %s"
+                          %(code.upper(),r[0],r[1],r[2],hex(tester.getcrc())))
+            if not err == 0: errors += 1
+            print("\n")
+        except: break
+    if not errors == 0:
+        print("\n--There where %d errors!--\n"%(errors))
+    else:
+        print("\n--No errors found--\n")
+    for lenght in codeLenghts.keys():
+        print("Base 36 have had %d codes with lenght %d\n"%(codeLenghts[lenght],lenght))
+
+def main():#arg0,arg1,arg2):
+    parser = OptionParser()
     
+    parser.add_option("-g","--generate",
+                      help="Used to generate codes giving 3 integer arguments (O..O,B..B,A..A)\n"\
+                           "max Ofert=131071,bonus=65535,attr=128")
+    parser.add_option("-v", "--validate",
+                      help="Used to validate a given code")
+    parser.add_option("-k","--key",
+                      help="Hexadecimal value of the key")
+    parser.add_option("-d", "--debug",
+                      help="Set debug mode",default=False)
+    
+    parser.add_option("-l","--loop",
+                      help="Test the ticket system with in a loop of random values",default=NLOOPS)
+    
+    (options, args) = parser.parse_args()
+    debug = options.debug
+    
+    if not options.loop:
+        if not options.key:
+            print("\n\tImpossible to work without \"key\" parameter\n")
+            exit(-1)
+        if not options.generate and not options.validate:
+            print("\n\tImpossible to work without one of the \"generate\" or \"validate\" options\n")
+            exit(-2)
+        key = convertKey(options.key)
+        if options.generate:
+            generator = BarCode(int(key),options.debug)
+            ofert,bonus,attrs = options.generate.split(",")
+            code = generator.generateCode(int(ofert), int(bonus), int(attrs))
+            print("\nFor Ofert: %s\tBonus:%s\twith attrs:%s (crc %s)\n\t"\
+                  "The code is %s (base36)(len %d)"
+                  %(ofert,bonus,attrs,hex(generator.getcrc()),code.upper(),len(code)))
+        else:
+            validator = BarCode(int(key),options.debug)
+            code = options.validate
+            try:
+                r = validator.validateCode(code)
+            except Exception,e:
+                print("Validation exception: %s"%(e))
+            else:
+                nOfert,nBonus,attrs = r
+                print("Valid: ofert: %d, bonus: %d, attributes: %d, crc: %s\n"
+                      %(nOfert,nBonus,attrs,hex(validator.getcrc())))
+    else:
+        generateAndValidateLoop(int(options.loop),options.debug)
+
 if __name__ == "__main__":
-    import sys
-    import traceback
-    arg0 = sys.argv[0]
-    if len(sys.argv) < 2 or sys.argv[1] == "--help":
-        arg1 = "--help"
-    else:
-        if sys.argv[1] == '--loop':
-            arg1 = [sys.argv[1],None]
-        else:
-            arg1 = [sys.argv[1],sys.argv[2]]
-    if len(sys.argv) == 4 and sys.argv[3] == "--debug":
-        arg2 = True
-    else:
-        arg2 = False
-    try:
-        main(arg0,arg1,arg2)
-    except Exception,e:
-        print("\nException for option %s, with reason %s\n"%(arg1,e))
-        traceback.print_exc()
-
-
-    
+    main()
