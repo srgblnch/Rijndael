bf4fedb62f90ef2fef3200ef4f9d24d24e230e0b
srgblnch <srgblnch@gmail.com>
2014-07-13 21:25:31 +0200

    sort the constructor. Introduce some TODO and FIXME tags. Bugfix the add opeartion.

--- algebra/polynomials.py
+++ algebra/polynomials.py
@@ -21,6 +21,8 @@
 ##
 ##############################################################################
 
+from copy import copy
+
 class Polynomial:
     def __init__(self,value,variable='x'):
         '''Polynomial defined by coefficients in a field or a ring.
@@ -28,19 +30,27 @@ class Polynomial:
            Example: polynomials.Polynomial([1,0,2,3],variable='z')
            means: z^3 + 2z^1 + 3
         '''
+        #TODO: it must allow to define a polynomial over more than the integers
+        #      that is, for example, a finite field or ring, then the
+        #      comparisons with 1 or 0, must change to request that field who 
+        #      is its identity elements in the additive operation and which one
+        #      in the multiplicative operation.
         self._variable = variable
-        if type(value) == list and len(value)>0:
-            self._coefficients = value
-            #remove null elements on the left side coefficients
-            while self._coefficients[0] == 0:
-                self._coefficients.pop(0)
-            #check all remaining elements have same integer type
-            for i in range(len(self._coefficients)):
-                if type(self._coefficients[0]) != type(self._coefficients[i]):
-                    raise ValueError("All the polynomial coefficients must "\
-                                     "be the same type")
-        elif type(value) == Polynomial:
+        if type(value) == Polynomial:
             self._coefficients = value.coefficients
+        elif type(value) == list:
+            if (value.count(0) == len(value)):
+                self._coefficients = []
+            elif len(value)>0:
+                self._coefficients = value
+                #remove null elements on the left side coefficients
+                while self._coefficients[0] == 0:
+                    self._coefficients.pop(0)
+                #check all remaining elements have same integer type
+                for i in range(len(self._coefficients)):
+                    if type(self._coefficients[0]) != type(self._coefficients[i]):
+                        raise ValueError("All the polynomial coefficients must "\
+                                         "be the same type")
         else:
             self._coefficients = []
     @property
@@ -66,13 +76,24 @@ class Polynomial:
             #FIXME: review this naming
             for exponent,coefficient in enumerate(self._coefficients):
                 if not coefficient == 0:
+                    #FIXME: Improve this dirted casuistry...
                     if self.degree-exponent > 0:
-                        cR.append("(%s%s^%d)"
-                           %(coefficient if coefficient != 1 else '',
-                             self._variable,self.degree-exponent))
+                        cR.append("%s%s%s"
+                           %('' if coefficient == 0 else \
+                             '+' if coefficient == 1 else \
+                             '+%s*'%(coefficient) if coefficient > 1 else \
+                             '%s*'%(coefficient),
+                             self._variable,
+                             '' if exponent == 1 else \
+                             '^%d'%(self.degree-exponent)))
                     else:
-                        cR.append('%s'%(coefficient))
-            return '+'.join(["%s"%(r) for r in cR])
+                        cR.append('' if coefficient == 0 else\
+                                  '+%s'%(coefficient) if coefficient > 0 else\
+                                  '%s'%(coefficient))
+            joining = ''.join(["%s"%(r) for r in cR])
+            if joining[0] == '+':
+                joining = joining[1:]
+            return joining
     def __abs__(self):
         return len(self._coefficients)
     def __len__(self):
@@ -80,8 +101,8 @@ class Polynomial:
     #@checkTypes#FIXME: decorator with arguments
     def __add__(self,other):
         self.checkTypes(other)
-        a = self.coefficients
-        b = other.coefficients
+        a = copy(self.coefficients)
+        b = copy(other.coefficients)
         if self.degree > other.degree:
             diff = self.degree - other.degree
             b = [0]*diff+b
@@ -91,6 +112,7 @@ class Polynomial:
         s = []
         for i in range(self.degree+1):
             s.append(a[i]+b[i])
+        print s
         return Polynomial(s,variable=self.variable)
     def __neg__(self):
         return Polynomial([-a for a in self.coefficients],variable=self.variable)
@@ -102,20 +124,25 @@ class Polynomial:
         return self.__iter__()
     #@checkTypes
     def __mul__(self,other):
+        #FIXME: more test needed
         self.checkTypes(other)
         if self.isZero() or other.isZero():
             return Polynomial([],variable=self.variable)
         m = [0]*(self.degree+other.degree+2)
         print("m = %s"%(m))
-        a = self.coefficients
+        a = copy(self.coefficients)
         a.reverse()
         print("a = %s"%(a))
-        b = self.coefficients
+        b = copy(other.coefficients)
         b.reverse()
         print("b = %s"%(b))
         for i in range(len(a)):
             for j in range(len(b)):
                 print("m[%d] = a[%d]+b[%d] = %s + %s = %s"%(i+j,i,j,a[i],b[j],m[i+j]))
                 m[i+j] += a[i]*b[j]
+        m.reverse()
         print("m = %s"%(m))
         return Polynomial(m,variable=self.variable)
+    #TODO: multiplicative inverse
+    #TODO: division
+    #TODO: modular division
