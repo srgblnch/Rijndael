09aa795645a51d89ee137439de8eab07bcefb442
Sergi Blanch-Torné <sergi@calcurco.cat>
2014-10-13 12:24:54 +0200

    iniciate a Rijndael's generalization refactoring

---
+++ GeneralizedRijndael/GeneralizedRijndael.pyx
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+#---- licence header
+##############################################################################
+##
+## file: GeneralizedRijndael.pyx
+##
+## developers history & copyleft: Sergi Blanch-Torne
+##
+## Copyright 2011, 2012, 2013, 2014 (copyleft)
+##
+## This file is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This file is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with this file.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+class GeneralizedRijndael:
+    def __init__(self):
+        pass
